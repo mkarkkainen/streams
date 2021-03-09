4a38497877daac3b9f372c17cc6b70ccd4bd7af4
Miikka <miikkak@tuta.io>
2021-03-09 09:26:18 -0600

    streaming works

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -11,10 +11,24 @@ class StreamShow extends React.Component {
   }
   componentDidMount() {
     const { id } = this.props.match.params;
+
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
-      type: "flv",
-      url: `http://localhost:8000/live/${id}`,
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
